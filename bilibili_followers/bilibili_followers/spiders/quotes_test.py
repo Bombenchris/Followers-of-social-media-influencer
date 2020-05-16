0c61fae486e7e889414d7abe80f5b8ca1b4bc6f6
Cheng Li <lichengchris@gmail.com>
2020-05-16 14:47:14 +0200

    update data

--- bilibili_followers/bilibili_followers/spiders/quotes_test.py
+++ bilibili_followers/bilibili_followers/spiders/quotes_test.py
@@ -7,14 +7,15 @@ from ..items import BilibiliFollowersItem
 class FollowerSpider(scrapy.Spider):
     name = 'followers'
     start_urls = [
-        'https://www.bilibili.com/v/music/other/'
+        # 'https://www.bilibili.com/v/douga/other/' # Animation others
+        'https://www.bilibili.com/v/douga/mad/'  # Animation MAD.AMV
     ]
     headers = {
         "accept": "*/*",
         "cache-control": "no-cache",
         "accept-encoding": "gzip, deflate, br",
         "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
-        "referer": "https://www.bilibili.com/v/music/other/",
+        "referer": "{}".format(start_urls[0]),
         "cookie": "bsource=seo_google; _uuid=1B06E8DC-4261-4F4F-E7EF-8641AD24200691958infoc; buvid3=27774F2D-C97D-4758-A45B-FFC63740895A155830infoc; PVID=1; CURRENT_FNVAL=16; sid=6rsp66f6",
         "sec-fetch-dest": "script",
         "sec-fetch-mode": "no-cors",
@@ -25,15 +26,15 @@ class FollowerSpider(scrapy.Spider):
     items = BilibiliFollowersItem()
 
     def parse(self, response):
-        callback = 'jqueryCallback_bili_35071251941392223'
+        callback = 'jqueryCallback_bili_9774461024721197'
         # page = 1
         pagesize = '20'
-        time_from = '20200301'
-        time_to = '20200331'
-        cate_id = '28'  # different tag chanel id, music_others(130),  music_original(28)
+        time_from = '20200101'
+        time_to = '20200401'
+        cate_id = '24'  # different tag chanel id, music_others(130),  music_original(28) douga/other(27)
 
-        # from page 1 to 100, 20 items per page
-        for i in range(100):
+        # from page 1 to 500, 20 items per page
+        for i in range(500):
             # url = 'https://api.bilibili.com/x/relation/followers?vmid=546195&pn=1&ps=20&order=desc'
             url = 'https://s.search.bilibili.com/cate/search?callback=' + callback + '&search_type=video&view_type=hot_rank' \
                   + '&order=click&copy_right=-1&cate_id=' + cate_id + '&page=' + str(FollowerSpider.page_n) + '&pagesize=' \
--- bilibili_followers/dataprocessor.py
+++ bilibili_followers/dataprocessor.py
@@ -1,32 +1,40 @@
 import sqlite3
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 def read_from_db(curr):
-    curr.execute('SELECT * FROM bilibili_db ORDER BY FOLLOWERS DESC')
-    data = c.fetchall()
-    n = len(data)
-    user_sort = range(1, n + 1)
+    curr.execute('SELECT COUNT(DISTINCT USER) FROM bilibili_db')
+    n = curr.fetchone()[0]  # total number of user
+    n_select = 100
+    curr.execute('SELECT * FROM bilibili_db ORDER BY FOLLOWERS DESC LIMIT {}'.format(n_select))
+    data = curr.fetchall()
+    user_sort = range(1, n_select + 1)
     follower_num = []
     for row in data:
         follower_num.append(row[1])
-    return user_sort, follower_num
+    return user_sort, follower_num, n
 
-database = 'bilibili_music_original_01032020_31032020.db'
+
+database = 'bilibili_douga_other_01012020_01042020.db'
 conn = sqlite3.connect(database)
 # conn = sqlite3.connect('bilibili_music_others_01032020_31032020.db')
 c = conn.cursor()
-USER_SORT, FOLLOWER_NUM = read_from_db(c)
-# fig = plt.figure()
-# ax = plt.gca()
-# ax.scatter(USER_SORT, FOLLOWER_NUM)
-# ax.set_yscale('log')
-# ax.set_xscale('log')
+USER_SORT, FOLLOWER_NUM, N = read_from_db(c)
+log_USER_SORT = np.log(USER_SORT)
+log_FOLLOWER_NUM = np.log(FOLLOWER_NUM)
 
-plt.loglog(USER_SORT, FOLLOWER_NUM, 'o', markersize=2)
+LP_model = np.polyfit(log_USER_SORT, log_FOLLOWER_NUM, deg=1)
+slope_log, intercept_log = LP_model
+FOLLOWER_predict = np.poly1d(LP_model)
+FOLLOWER_NUM_LP = np.exp(FOLLOWER_predict(log_USER_SORT))
+
+plt.loglog(USER_SORT, FOLLOWER_NUM, 'o', markersize=2, label='log-log')
+line_fit = plt.loglog(USER_SORT, FOLLOWER_NUM_LP, label="k={:.2f}".format(slope_log), color='r')
+plt.legend(loc='upper right')
 plt.xlabel('rank')
 plt.ylabel('Followers')
-plt.title(database+'_N={}'.format(len(USER_SORT)))
+plt.title(database + '_N={}'.format(N))
 plt.show()
 
 plt.plot
